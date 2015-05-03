96337da454204fc540d1d9b878dd408030cd9dcf
Duncan Marsh <dwolfm@gmail.com>
2015-05-02 18:04:36 -0700

    added color converters to transform.js

--- test/transform_test.js
+++ test/transform_test.js
@@ -4,9 +4,24 @@ var transform = require('../lib/transform.js');
 var expect = require('chai').expect;
 
 describe('transfrom.js', function(){
-	var testColor = {red: 110, green: 200, blue:222, alpha:1};
+	var testColor = {red: 30, green: 20, blue:25, alpha:255};
 
-	console.log('org obj: ' + testColor);
-	console.log('dodge 1: ' + transform.dodge(testColor));
-	console.log('dodge 100: ' + transform.dodge(testColor, 100));
+	console.log( testColor);
+	console.log('dodge 1');
+  	console.log(transform.dodge(testColor));
+	console.log('dodge 100');
+	console.log(transform.dodge(testColor, 100));
+	console.dir('invert');
+  	console.log(transform.invert(testColor));
+	console.log('burn 1');
+	console.log(transform.burn(testColor));
+	console.log('burn 100');
+	console.log(transform.burn(testColor, 100));
+	console.log(transform.setRGBA(55,33	,55,255));
+	var hsl = transform.rgbToHsl(testColor);
+	console.log('rgbToHsl');
+	console.log(hsl);
+	console.log('hslToRgb');
+	console.log(transform.hslToRgb(hsl));
 });
+
